84e39a63740c77ea06c4298b8e21933283aadc7c
Diego <A01745336@itesm.mx>
2022-04-23 09:20:55 -0500

    Brazil Casi Terminado

--- Assets/Brazil/Scripts/GameManager.cs
+++ Assets/Brazil/Scripts/GameManager.cs
@@ -1,12 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class GameManager : MonoBehaviour
 {
     public SpriteRenderer[] instruments;
+    public AudioSource[] buttonSounds;
 
     private int instrumentSelect;
+
+    public float stayLit;
+    private float stayLitCounter;
+
+    public float waitBetweenLights;
+    private float waitBetweenCounter;
+
+    private bool shouldBeLit;
+    private bool shouldBeDark;
+
+    public List<int> activeSequence;
+    private int positionInSequence;
+
+    private bool gameActive;
+    private int inputInSequence;
+
+    public AudioSource correct;
+    public AudioSource incorrect;
+
+    public TextMeshProUGUI scoreText;
+
     void Start()
     {
         
@@ -15,13 +38,100 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(shouldBeLit)
+        {
+            stayLitCounter -= Time.deltaTime;
+            if (stayLitCounter < 0)
+            {
+                instruments[activeSequence[positionInSequence]].color = new Color(instruments[activeSequence[positionInSequence]].color.r, instruments[activeSequence[positionInSequence]].color.g, instruments[activeSequence[positionInSequence]].color.b, 0.7f);
+                buttonSounds[activeSequence[positionInSequence]].Stop();
+                shouldBeLit = false;
+
+                shouldBeDark = true;
+                waitBetweenCounter = waitBetweenLights;
+
+                positionInSequence++;
+            }
+        }
+        if (shouldBeDark)
+        {
+            waitBetweenCounter -= Time.deltaTime;
+            
+            if(positionInSequence >= activeSequence.Count)
+            {
+                shouldBeDark = false;
+                gameActive = true;
+            }
+            else
+            {
+                if (waitBetweenCounter < 0)
+                {
+
+                    instruments[activeSequence[positionInSequence]].color = new Color(instruments[activeSequence[positionInSequence]].color.r, instruments[activeSequence[positionInSequence]].color.g, instruments[activeSequence[positionInSequence]].color.b, 1f);
+                    buttonSounds[activeSequence[positionInSequence]].Play();
+
+                    stayLitCounter = stayLit;
+                    shouldBeLit = true;
+                    shouldBeDark = false;
+                }
+            }
+        }
     }
 
     public void StartGame()
     {
+        activeSequence.Clear();
+
+        positionInSequence = 0;
+        inputInSequence = 0;
+
         instrumentSelect = Random.Range(0, instruments.Length);
 
+        activeSequence.Add(instrumentSelect);
+
+        instruments[activeSequence[positionInSequence]].color = new Color(instruments[activeSequence[positionInSequence]].color.r, instruments[activeSequence[positionInSequence]].color.g, instruments[activeSequence[positionInSequence]].color.b, 1f);
+        buttonSounds[activeSequence[positionInSequence]].Play();
         
+        stayLitCounter = stayLit;
+        shouldBeLit = true;
+    }
+
+    public void ColorPressed(int whichButton)
+    {
+        if (gameActive)
+        {
+
+            if (activeSequence[inputInSequence] == whichButton)
+            {
+                Debug.Log("Correct");
+                correct.Play();
+
+                inputInSequence++;
+
+                if(inputInSequence >= activeSequence.Count)
+                {
+                    positionInSequence = 0;
+                    inputInSequence = 0;
+
+                    instrumentSelect = Random.Range(0, instruments.Length);
+
+                    activeSequence.Add(instrumentSelect);
+
+                    instruments[activeSequence[positionInSequence]].color = new Color(instruments[activeSequence[positionInSequence]].color.r, instruments[activeSequence[positionInSequence]].color.g, instruments[activeSequence[positionInSequence]].color.b, 1f);
+                    buttonSounds[activeSequence[positionInSequence]].Play();
+
+                    stayLitCounter = stayLit;
+                    shouldBeLit = true;
+
+                    gameActive = false;
+                }
+            }
+            else
+            {
+                Debug.Log("Wrong");
+                incorrect.Play();
+                gameActive = false;
+            }
+        }
     }
 }
